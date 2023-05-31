82c6fe914b1931293356b73590e071ec6505a847
Remy CASTRO <remycastro27@icloud.com>
2023-05-31 23:19:16 +0200

    Fix slider error

--- src/components/Trips/Tabs/General.tsx
+++ src/components/Trips/Tabs/General.tsx
@@ -1,17 +1,25 @@
-import Team from "@/components/Trips/Tabs/Team";
 import {useEffect, useState} from "react";
-import {Box, CircularProgress, Typography} from "@mui/material";
+import {Box, CircularProgress, Grid, Typography} from "@mui/material";
 import axios from "axios";
+import {Step} from "@prisma/client"
+import WidgetActivities from "@/components/Trips/Widgets/WidgetActivities";
+
+interface DataType {
+    start: Date,
+    end: Date,
+    steps: Array<Step>
+} 
 
 const General = ({tripId,isLoading, handleLoading} : {tripId: string, isLoading: boolean, handleLoading: (isLoading: boolean) => void}) => {
+    
+    const [data, setData] = useState<DataType>()
+    
     useEffect(() => {
         axios
             .get(`${process.env.NEXT_PUBLIC_APPURL}/api/trips/${tripId}/general`)
             .then(response => {
-                console.log(response)
-
+                setData(response.data.trip)
                 handleLoading(false)
-
             })
     }, [])
     
@@ -27,7 +35,11 @@ const General = ({tripId,isLoading, handleLoading} : {tripId: string, isLoading:
                 )
                 :
                 (
-                    <p>tab general</p>
+                    <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <WidgetActivities activities={data?.steps as Step[]} start={data?.start as Date} end={data?.end as Date} />
+                        </Grid>
+                    </Grid>
                 )
             }
         </>
