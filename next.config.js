e88a76a71c2b62a07fd5a45d50caa5f053a7660a
Remy CASTRO <remycastro27@icloud.com>
2023-07-05 18:24:18 +0200

    pwa test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
 module.exports = withPWA({
   experimental: {
     appDir: true,             // <---- Comment and Uncomment this
   },
-})
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,59 @@
-"use client" 
 
 import './globals.scss'
 import {Inter} from 'next/font/google'
 import Head from "next/head";
-import Pwa from "@/app/Pwa";
+import {Metadata} from "next";
 
 const inter = Inter({subsets: ['latin']})
+
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
+export const metadata: Metadata = {
+    applicationName: APP_NAME,
+    title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    manifest: "/manifest.json",
+    themeColor: "#FFFFFF",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: APP_DEFAULT_TITLE,
+        // startUpImage: [],
+    },
+    formatDetection: {
+        telephone: false,
+    },
+    openGraph: {
+        type: "website",
+        siteName: APP_NAME,
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: "summary",
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+};
 
 export default function RootLayout(props: any) {
     return (
             <html lang="en">
                 <Head>
                     <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
                     <meta name="theme-color" content="#fff" />
                 </Head>
                 <body className={inter.className}>
@@ -20,7 +62,6 @@ export default function RootLayout(props: any) {
                     </script>
                     {/* eslint-disable-next-line react/no-children-prop */}
                     {props.children}
-                    <Pwa/>
                 </body>
             </html>
     )
