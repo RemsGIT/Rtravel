4f9b1bee86270ff503892c661bbf70261eec97c1
Remy CASTRO <remycastro27@icloud.com>
2023-07-06 18:00:14 +0200

    Fix sign in loading bug

--- src/components/layouts/protectedLayouts.tsx
+++ src/components/layouts/protectedLayouts.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react';
 import {redirect, useRouter} from 'next/navigation';
 import { useEffect } from 'react';
 import LoadingPageSpinner from "@/components/LoadingPageSpinner";
+import {Box} from "@mui/material";
 
 type Props = {
   children: React.ReactElement;
@@ -20,14 +21,13 @@ export const ProtectedLayout = ({ children }: Props): JSX.Element => {
   const { status: sessionStatus } = useSession();
   const authorized = sessionStatus === 'authenticated';
   const unAuthorized = sessionStatus === 'unauthenticated';
-  const loading = sessionStatus === 'loading';
-
   
-  if(!unAuthorized) {
-    redirect('/login')
+  // if the user refreshed the page or somehow navigated to the protected page
+  if(unAuthorized) {
+    router.push('/login')
   }
 
   // if the user is authorized, render the page
   // otherwise, render nothing while the router redirects him to the login page
-  return authorized ? <div>{children}</div> : <></>;
+  return authorized ? <div>{children}</div> : <LoadingPageSpinner />;
 };
