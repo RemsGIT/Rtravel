d0dfe4b3d85f057058932d01c0f2fb7ca552f642
Remy CASTRO <remycastro27@icloud.com>
2023-07-06 17:40:08 +0200

    Fix loading spinner page colours and weird behaviour of multiple redirects with white page

--- src/components/layouts/protectedLayouts.tsx
+++ src/components/layouts/protectedLayouts.tsx
@@ -22,20 +22,12 @@ export const ProtectedLayout = ({ children }: Props): JSX.Element => {
   const unAuthorized = sessionStatus === 'unauthenticated';
   const loading = sessionStatus === 'loading';
 
-  useEffect(() => {
-    // if the user is not authorized, redirect to the login page
-    // with a return url to the current page
-    if (unAuthorized) {
-      redirect('/login')
-    }
-  }, [loading, unAuthorized, sessionStatus, router]);
-
-  // if the user refreshed the page or somehow navigated to the protected page
-  if (loading) {
-    return <LoadingPageSpinner/>;
+  
+  if(!unAuthorized) {
+    redirect('/login')
   }
 
   // if the user is authorized, render the page
   // otherwise, render nothing while the router redirects him to the login page
-  return authorized ? <div>{children}</div> : <LoadingPageSpinner />;
+  return authorized ? <div>{children}</div> : <></>;
 };
