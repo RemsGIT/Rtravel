1c23fa987f5da8e8a3602b6ea3fcb767ffa1efcf
Remy CASTRO <remycastro27@icloud.com>
2023-07-10 13:12:56 +0200

    Fix options select city no visible

--- src/components/Utils/SelectCity.tsx
+++ src/components/Utils/SelectCity.tsx
@@ -5,33 +5,34 @@ import {ChangeEvent, useEffect, useState} from "react";
 const SelectCity = ({handleOnChange, resetText, defaultCity = '', isDeleting = false}  : {handleOnChange: (value: string) => void, resetText?: boolean, defaultCity?: string, isDeleting?: boolean}) => {
     const [predictions, setPredictions] = useState<string[]>([]);
     const [value, setValue] = useState<string | null>(defaultCity);
+    
+    //@ts-ignore
+    const service = new google.maps.places.AutocompleteService()
 
     const searchDestination = (textTyped: string) => {
 
-        if(textTyped.trim().length <= 0) {
-            setPredictions([])
-
-            return
-        }
-
-        //@ts-ignore
-        const service = new google.maps.places.AutocompleteService()
-
-        const req = {
-            types: ['(cities)'],
-            input: textTyped,
-            language: 'fr'
-        }
-
-        service.getPlacePredictions(req, (resultPredictions: any) => {
-            if(resultPredictions !== null) {
-                setPredictions(resultPredictions.map((rp: any) => `${rp.structured_formatting.main_text}, ${rp.structured_formatting.secondary_text}`))
-            }
-            else {
+            if(textTyped.trim().length <= 0) {
                 setPredictions([])
+
+                return
             }
-        })
-    }
+
+            const req = {
+                types: ['(cities)'],
+                input: textTyped,
+                language: 'fr'
+            }
+
+            service.getPlacePredictions(req, (resultPredictions: any) => {
+                if(resultPredictions !== null) {
+                    setPredictions(resultPredictions.map((rp: any) => `${rp.structured_formatting.main_text}, ${rp.structured_formatting.secondary_text}`))
+                }
+                else {
+                    setPredictions([])
+                }
+            })
+}
+        
     
     useEffect(() => {
         setValue(defaultCity)
@@ -54,6 +55,7 @@ const SelectCity = ({handleOnChange, resetText, defaultCity = '', isDeleting = f
             onChange={(event: any, newValue: string | null) => {
                 handleOnChange(newValue ?? '')
             }}
+            disablePortal={true}
             options={predictions}
             disabled={isDeleting}
         />
